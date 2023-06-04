bd686949f478bcaecab95636d5f6b743ac975bbd
keremcakmakx <95014971+keremcakmakx@users.noreply.github.com>
2023-06-04 18:24:05 +0300

    animateCell

--- Assets/Scripts/Game/Grid/CellController.cs
+++ Assets/Scripts/Game/Grid/CellController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CellController : MonoBehaviour
@@ -9,6 +10,7 @@ public class CellController : MonoBehaviour
     public Vector2Int GridInfo { get; private set; }
     private BoxCollider2D _collider;
 
+    private Sequence _sequence;
     public void Initialize(Vector2Int gridInfo, float size)
     {
         gameObject.SetActive(true);
@@ -23,8 +25,27 @@ public class CellController : MonoBehaviour
 
     public void SetFull(bool full)
     {
+        if (full == IsFull)
+            return;
+
         IsFull = full;
+
         fullCell.SetActive(IsFull);
         defaultCell.SetActive(!IsFull);
+
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
+
+        if (IsFull)
+            AnimateScaling(fullCell, Vector3.one, 0.3f);
+        else
+            AnimateScaling(defaultCell, Vector3.one, 0.45f);
+    }
+
+    private void AnimateScaling(GameObject target, Vector3 targetScale, float duration)
+    {
+        target.transform.localScale = 0.5f * targetScale;
+        _sequence.Append(target.transform.DOScale(targetScale, duration).SetEase(Ease.OutQuart));
+        _sequence.Play();
     }
 }
